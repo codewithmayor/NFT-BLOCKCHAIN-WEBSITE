4549e7115bf7c224dee53d800b7a30efcb190cc3
Replit user <>
2023-03-09 20:13:40 +0000

    update

---
+++ replit.nix
@@ -0,0 +1,8 @@
+{ pkgs }: {
+  deps = [
+    pkgs.python39Packages.flask
+    pkgs.python39Packages.pip
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server  
+  ];
+}
